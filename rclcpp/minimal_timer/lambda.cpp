1c499f389a382766bd73c7c43f2753dfed96b9eb
Donghee Ye <donghee.ye@samsung.com>
2020-03-09 11:23:43 +0900

    Add code to test timer bug when reset after cancel

--- rclcpp/minimal_timer/lambda.cpp
+++ rclcpp/minimal_timer/lambda.cpp
@@ -14,6 +14,8 @@
 
 #include <chrono>
 #include <memory>
+#include <glib.h>
+
 
 #include "rclcpp/rclcpp.hpp"
 
@@ -30,18 +32,53 @@ public:
   MinimalTimer()
   : Node("minimal_timer")
   {
-    auto timer_callback = [this]() -> void {RCLCPP_INFO(this->get_logger(), "Hello, world!");};
+    auto timer_callback = [this]() -> void {
+      RCLCPP_INFO(this->get_logger(), "Timer cancel");
+      timer_->cancel();
+    };
     timer_ = create_wall_timer(500ms, timer_callback);
   }
 
-private:
+//private:
   rclcpp::TimerBase::SharedPtr timer_;
 };
+
+GMainLoop *mainloop;
+std::shared_ptr<rclcpp::executors::SingleThreadedExecutor> exec;
+std::shared_ptr<MinimalTimer> node;
+
+
+gboolean TimerExpired(gpointer data) {
+   if (node->timer_->is_canceled()){
+     RCLCPP_INFO(node->get_logger(), "Timer was cancelled and reset");
+     node->timer_->reset();
+   }else {
+     RCLCPP_INFO(node->get_logger(), "Timer was not cancelled");
+   }
+   return true;
+}
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalTimer>());
+
+  exec = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
+  node = std::make_shared<MinimalTimer>();
+  exec->add_node(node);
+
+  mainloop = g_main_loop_new(nullptr, false);
+
+  g_timeout_add(1000, TimerExpired, nullptr);
+
+  g_thread_new(nullptr,
+        [](gpointer data) -> gpointer {
+          exec->spin();
+          return nullptr;
+        },
+        static_cast<void *>(node.get()));
+
+  g_main_loop_run(mainloop);
+
   rclcpp::shutdown();
   return 0;
 }
